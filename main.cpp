c9961976e2352f8f323cca6090365ff931334f75
Carolina <bruscantinic@gmail.com>
2016-01-19 23:24:15 -0300

    Primer commit - Hello world!

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Carolina
+ *
+ * Created on 19 de enero de 2016, 23:20
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
